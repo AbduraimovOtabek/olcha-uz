d05a846bc41412e61a184d1e11966b52bbc87158
otabek <abduraimov>
2023-06-16 10:22:41 +0500

    first commit

---
+++ src/main/java/com/company/olchauz/OlchaUzApplication.java
@@ -0,0 +1,13 @@
+package com.company.olchauz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlchaUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlchaUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/olchauz/OlchaUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.olchauz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlchaUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
